c8241fa893e1c11af314de60b025abc49e327e8f
Gugui80 <manuelodoghertyzufia@gmail.com>
2023-04-11 16:21:51 +0200

    Practica node

---
+++ NODE/practica3-manejadores/main.js
@@ -0,0 +1,6 @@
+//Declaramos un manejador de enrutamientos de las url de las peticiones a nuestro servidor.
+const routerIgae = ( pathPeticion ) => {
+    console.log('Petición enrutada para ' + pathPeticion);
+}
+
+exports.routerIgae = routerIgae;
